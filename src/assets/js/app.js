dcafa6c79fbd1294523ff4d6e69c34264375fd3b
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2017-07-27 17:45:09 +0200

    Implements debug feature

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,5 +6,11 @@ window.addEventListener("deviceorientation", function (e) {
 
   // document.getElementById("content").innerHTML =  "gamma: " + y + "<br />";
 
+  debugx.innerHTML = "x: " + x;
+  debugy.innerHTML = "y: " + y;
+
+  if (x >  90) { x =  90};
+  if (x < -90) { x = -90};
+
   document.getElementById("content").style.transform = "rotate3d(1, 0, 0, " + x + "deg)";
 }, true);
